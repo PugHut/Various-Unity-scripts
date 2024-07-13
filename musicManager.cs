46ec6675f784593a34caaafa83bd26343aef304b
PugHut <138637231+PugHut@users.noreply.github.com>
2024-07-12 21:16:23 -0700

    Add files via upload

---
+++ musicManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour
+{
+    public bool guitarCheck = true;
+    public bool bassCheck;
+    public bool vocal1Check;
+    public bool vocal2Check;
+    public bool drumsCheck;
+
+    public AudioSource Guitar;
+    public AudioSource Bass;
+    public AudioSource Vocals1;
+    public AudioSource Vocals2;
+    public AudioSource Drums;
+
+    //Every frame update. Plz make it not every frame that may cause problems in the future :3
+    void Update()
+    {
+        //Guitar Checks
+        if (guitarCheck == true)
+        {
+            Guitar.volume = 1;
+        }
+        else if (guitarCheck == false)
+        {
+            Guitar.volume = 0;
+        }
+        //Bass Checks
+        if (bassCheck == true)
+        {
+            Bass.volume = 1;
+        }
+        else if (bassCheck == false)
+        {
+            Bass.volume = 0;
+        }
+        //Drums Checks
+        if (drumsCheck == true)
+        {
+            Drums.volume = 1;
+        }
+        else if (drumsCheck == false)
+        {
+            Drums.volume = 0;
+        }
+        //Vocal Checks
+        if (vocal1Check == true)
+        {
+            Vocals1.volume = 1;
+        }
+        else if (vocal1Check == false)
+        {
+            Vocals1.volume = 0;
+        }
+
+        if (vocal2Check == true)
+        {
+            Vocals2.volume = 1;
+        }
+        else if (vocal2Check == false)
+        {
+            Vocals2.volume = 0;
+        }
+    }
+}
